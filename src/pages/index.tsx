3953073a900e8273ae21e5d932c4de9e2266b839
kstulgys <karolis.stulgys@gmail.com>
2023-03-23 22:07:56 +0200

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home({ data }: any) {
   return <HomePage {...data} />;
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const entry = matter.read('./content/pages/home.md');
   return {
     props: { data: entry.data },
